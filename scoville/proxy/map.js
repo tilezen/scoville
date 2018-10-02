4862fa75750733c96385bafdc91f778bd4b6c709
Matt Amos <zerebubuth@gmail.com>
2018-10-02 16:10:03 +0100

    Proper attribution on Stamen Toner tiles.

--- scoville/proxy/map.js
+++ scoville/proxy/map.js
@@ -4,6 +4,7 @@ L.tileLayer('http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png', {
     retina: true,
     subdomains: 'abcd',
     maxZoom: 16,
+    attribution: 'Map tiles by <a href="http://stamen.com/">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org/">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.'
 }).addTo(map);
 
 L.tileLayer('http://localhost:{{ port }}/tiles/{z}/{x}/{y}.png', {
